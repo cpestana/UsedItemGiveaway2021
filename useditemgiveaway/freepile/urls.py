a0089e84b95c42d029185e6e2af9f6a8f5fc7d17
Christine Pestana <clpestanaa@gmail.com>
2021-03-14 13:23:08 -0700

    first views commit

--- useditemgiveaway/freepile/urls.py
+++ useditemgiveaway/freepile/urls.py
@@ -1,12 +1,11 @@
-from django.contrib import admin 
 from django.urls import path
 from . import views
-from django.config.urls import include
 
-urlpatters = {
-path('', view.index, name='index'), 
+
+urlpatterns = [
+path('', views.index, name='index'), 
 path('Items/', views.Items, name='Item'), 
-path('Comments/', view.Comments, name='Comment'), 
+path('Comments/', views.Comments, name='Comment'), 
 
-}
+]
 
--- useditemgiveaway/freepile/views.py
+++ useditemgiveaway/freepile/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from .models import Items, Comments
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
 
 # Create your views here.
+def index(request):
+    return render(request, 'freepile/index.html')
+
+def getItems(request, id):
+    Items_list=get_object_or_404(Items, pk=id)
+    return render(request, 'freepile/items.html', {'Items_list': Items_list} )
+
+def getComments(request, id):
+    Comments_list=Comments.objects.all()
+    return render(request, 'freepile/comments.html', {'Comments_list': Comments_list})
